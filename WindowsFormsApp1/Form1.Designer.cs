3008684b65aa7e305d94e934e9ea879a22c89b96
Administrator <Administrator@HEYUAN-9955HX>
2026-02-22 09:24:13 +0800

    添加项目文件。

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_count = new System.Windows.Forms.Button();
+            this.txt_input1 = new System.Windows.Forms.TextBox();
+            this.txt_input2 = new System.Windows.Forms.TextBox();
+            this.txt_result = new System.Windows.Forms.TextBox();
+            this.cbx_operator = new System.Windows.Forms.ComboBox();
+            this.lbx_operatore = new System.Windows.Forms.ListBox();
+            this.lbx_history = new System.Windows.Forms.ListBox();
+            this.tcl_1 = new System.Windows.Forms.TabControl();
+            this.tabPage_count = new System.Windows.Forms.TabPage();
+            this.but_clear = new System.Windows.Forms.Button();
+            this.tabPage_history = new System.Windows.Forms.TabPage();
+            this.btn_his_clear = new System.Windows.Forms.Button();
+            this.tcl_1.SuspendLayout();
+            this.tabPage_count.SuspendLayout();
+            this.tabPage_history.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_count
+            // 
+            this.btn_count.Location = new System.Drawing.Point(482, 298);
+            this.btn_count.Name = "btn_count";
+            this.btn_count.Size = new System.Drawing.Size(121, 64);
+            this.btn_count.TabIndex = 0;
+            this.btn_count.Text = "计算";
+            this.btn_count.UseVisualStyleBackColor = true;
+            this.btn_count.Click += new System.EventHandler(this.btn_count_Click);
+            // 
+            // txt_input1
+            // 
+            this.txt_input1.Location = new System.Drawing.Point(35, 52);
+            this.txt_input1.Name = "txt_input1";
+            this.txt_input1.Size = new System.Drawing.Size(152, 28);
+            this.txt_input1.TabIndex = 1;
+            // 
+            // txt_input2
+            // 
+            this.txt_input2.Location = new System.Drawing.Point(482, 52);
+            this.txt_input2.Name = "txt_input2";
+            this.txt_input2.Size = new System.Drawing.Size(152, 28);
+            this.txt_input2.TabIndex = 2;
+            // 
+            // txt_result
+            // 
+            this.txt_result.Location = new System.Drawing.Point(26, 191);
+            this.txt_result.Name = "txt_result";
+            this.txt_result.Size = new System.Drawing.Size(608, 28);
+            this.txt_result.TabIndex = 3;
+            // 
+            // cbx_operator
+            // 
+            this.cbx_operator.FormattingEnabled = true;
+            this.cbx_operator.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "×",
+            "÷"});
+            this.cbx_operator.Location = new System.Drawing.Point(261, 54);
+            this.cbx_operator.Name = "cbx_operator";
+            this.cbx_operator.Size = new System.Drawing.Size(121, 26);
+            this.cbx_operator.TabIndex = 4;
+            // 
+            // lbx_operatore
+            // 
+            this.lbx_operatore.FormattingEnabled = true;
+            this.lbx_operatore.ItemHeight = 18;
+            this.lbx_operatore.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "×",
+            "÷"});
+            this.lbx_operatore.Location = new System.Drawing.Point(261, 109);
+            this.lbx_operatore.Name = "lbx_operatore";
+            this.lbx_operatore.Size = new System.Drawing.Size(120, 76);
+            this.lbx_operatore.TabIndex = 5;
+            this.lbx_operatore.SelectedIndexChanged += new System.EventHandler(this.lbx_operatore_SelectedIndexChanged);
+            // 
+            // lbx_history
+            // 
+            this.lbx_history.FormattingEnabled = true;
+            this.lbx_history.ItemHeight = 18;
+            this.lbx_history.Location = new System.Drawing.Point(29, 22);
+            this.lbx_history.Name = "lbx_history";
+            this.lbx_history.Size = new System.Drawing.Size(486, 310);
+            this.lbx_history.TabIndex = 6;
+            this.lbx_history.SelectedIndexChanged += new System.EventHandler(this.lbx_history_SelectedIndexChanged);
+            // 
+            // tcl_1
+            // 
+            this.tcl_1.Controls.Add(this.tabPage_count);
+            this.tcl_1.Controls.Add(this.tabPage_history);
+            this.tcl_1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tcl_1.Location = new System.Drawing.Point(0, 0);
+            this.tcl_1.Name = "tcl_1";
+            this.tcl_1.SelectedIndex = 0;
+            this.tcl_1.Size = new System.Drawing.Size(733, 418);
+            this.tcl_1.TabIndex = 7;
+            // 
+            // tabPage_count
+            // 
+            this.tabPage_count.Controls.Add(this.but_clear);
+            this.tabPage_count.Controls.Add(this.txt_input1);
+            this.tabPage_count.Controls.Add(this.cbx_operator);
+            this.tabPage_count.Controls.Add(this.txt_result);
+            this.tabPage_count.Controls.Add(this.lbx_operatore);
+            this.tabPage_count.Controls.Add(this.btn_count);
+            this.tabPage_count.Controls.Add(this.txt_input2);
+            this.tabPage_count.Location = new System.Drawing.Point(4, 28);
+            this.tabPage_count.Name = "tabPage_count";
+            this.tabPage_count.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage_count.Size = new System.Drawing.Size(725, 386);
+            this.tabPage_count.TabIndex = 0;
+            this.tabPage_count.Text = "计算器";
+            this.tabPage_count.UseVisualStyleBackColor = true;
+            // 
+            // but_clear
+            // 
+            this.but_clear.Location = new System.Drawing.Point(482, 225);
+            this.but_clear.Name = "but_clear";
+            this.but_clear.Size = new System.Drawing.Size(121, 53);
+            this.but_clear.TabIndex = 6;
+            this.but_clear.Text = "清空";
+            this.but_clear.UseVisualStyleBackColor = true;
+            this.but_clear.Click += new System.EventHandler(this.but_clear_Click);
+            // 
+            // tabPage_history
+            // 
+            this.tabPage_history.Controls.Add(this.btn_his_clear);
+            this.tabPage_history.Controls.Add(this.lbx_history);
+            this.tabPage_history.Location = new System.Drawing.Point(4, 28);
+            this.tabPage_history.Name = "tabPage_history";
+            this.tabPage_history.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage_history.Size = new System.Drawing.Size(725, 386);
+            this.tabPage_history.TabIndex = 1;
+            this.tabPage_history.Text = "历史记录";
+            this.tabPage_history.UseVisualStyleBackColor = true;
+            // 
+            // btn_his_clear
+            // 
+            this.btn_his_clear.Location = new System.Drawing.Point(558, 22);
+            this.btn_his_clear.Name = "btn_his_clear";
+            this.btn_his_clear.Size = new System.Drawing.Size(142, 45);
+            this.btn_his_clear.TabIndex = 7;
+            this.btn_his_clear.Text = "清空历史记录";
+            this.btn_his_clear.UseVisualStyleBackColor = true;
+            this.btn_his_clear.Click += new System.EventHandler(this.btn_his_clear_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 418);
+            this.Controls.Add(this.tcl_1);
+            this.KeyPreview = true;
+            this.Name = "Form";
+            this.Text = "计算器";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form_KeyDown);
+            this.tcl_1.ResumeLayout(false);
+            this.tabPage_count.ResumeLayout(false);
+            this.tabPage_count.PerformLayout();
+            this.tabPage_history.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_count;
+        private System.Windows.Forms.TextBox txt_input1;
+        private System.Windows.Forms.TextBox txt_input2;
+        private System.Windows.Forms.TextBox txt_result;
+        private System.Windows.Forms.ComboBox cbx_operator;
+        private System.Windows.Forms.ListBox lbx_operatore;
+        private System.Windows.Forms.ListBox lbx_history;
+        private System.Windows.Forms.TabControl tcl_1;
+        private System.Windows.Forms.TabPage tabPage_count;
+        private System.Windows.Forms.TabPage tabPage_history;
+        private System.Windows.Forms.Button but_clear;
+        private System.Windows.Forms.Button btn_his_clear;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+    
+    
+        //点击计算按钮时，执行相应的运算，同时将计算过程添加到历史记录列表框中
+        private void btn_count_Click(object sender, EventArgs e)
+        {
+            //.Trim()去除首尾空格
+            //double.TryParse(需要转换的值,值转换后存放的地方)，执行后返回布尔值
+            bool bool_input1 = double.TryParse(txt_input1.Text.Trim(), out double input1);
+            bool bool_input2 = double.TryParse(txt_input2.Text.Trim(), out double input2);
+
+            //如果输入的值都能成功转换为数字，则根据选择的运算符执行相应的运算，并将结果显示在结果文本框中
+            if (bool_input1 == true && bool_input2 == true)
+            {
+                switch (cbx_operator.Text.Trim())
+                { 
+                    case "+":
+                        double output_add = input1 + input2;
+                        txt_result.Text = output_add.ToString();
+                        break;
+                    case "-":
+                        double output_minus = input1 - input2;
+                        txt_result.Text = output_minus.ToString();
+                        break;
+                    case "×":
+                        double output_mult = input1 * input2;
+                        txt_result.Text = output_mult.ToString();
+                        break;
+                    case "x":
+                        double output_mult1 = input1 * input2;
+                        txt_result.Text = output_mult1.ToString();
+                        break;
+                    case "÷":
+                        if (input2 != 0)
+                        {
+                            double output_div = input1 / input2;
+                            txt_result.Text = output_div.ToString();
+                        }
+                        else
+                        {
+                            MessageBox.Show("除数不能为0");
+                        }
+                        break;
+                    default:
+                        MessageBox.Show("请选择运算符");
+                        break;
+                }
+            }
+            else
+            {
+                txt_result.Text = "请在上面两个输入框中输入数字并选择运算符".Trim();
+                MessageBox.Show("请输入数字");
+            }
+
+
+            //将计算结果添加到历史记录列表框中
+            void AddToHistory()
+            {
+                string history_entry = $"{input1} {cbx_operator.Text.Trim()} {input2} = {txt_result.Text.Trim()}";
+                try
+                {
+                    if (!string.IsNullOrEmpty(history_entry))
+                    {
+                        lbx_history.Items.Add(history_entry);
+                    }
+                }
+                catch { }
+            }
+            AddToHistory();
+
+        }
+
+
+
+        //选择列表框中的运算符时，将其显示在组合框中
+        private void lbx_operatore_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //SelectedItem获取当前选中项的值，ToString()将其转换为字符串
+            cbx_operator.Text = lbx_operatore.SelectedItem.ToString();
+        }
+
+
+        //选择历史记录列表框中的某一项时，将其结果显示在结果文本框中，并将该历史记录复制到剪贴板
+        private void lbx_history_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           string history_result = lbx_history.SelectedItem.ToString();
+            txt_result.Text = history_result.Split('=')[1].Trim();
+
+            void copy_history()
+            {
+                try
+                {
+                    // 判断结果框是否为空，避免复制空内容报错
+                    if (!string.IsNullOrEmpty(txt_result.Text))
+                    {
+                        Clipboard.SetText(lbx_history.SelectedItem.ToString());
+                        //MessageBox.Show("复制成功！");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // 如果剪切板被占用或其他错误，捕获异常防止程序崩溃
+                    MessageBox.Show("复制失败，剪切板可能被其他程序占用。");
+                }
+                
+            }
+            copy_history();
+
+        }
+
+        //点击清除按钮时，清空输入框、结果框，并将输入焦点设置到第一个输入框中，方便用户继续输入
+        private void but_clear_Click(object sender, EventArgs e)
+        {
+            txt_input1.Clear();txt_input2.Clear();txt_result.Clear();
+
+            //Focus()方法将输入焦点设置到指定的控件上，这样用户就可以直接开始输入而不需要再点击一次输入框
+            txt_input1.Focus();
+
+        }
+
+        //点击历史记录清除按钮时，清空历史记录列表框
+        private void btn_his_clear_Click(object sender, EventArgs e){lbx_history.Items.Clear();}
+
+        /*
+        //在窗体加载时，将输入焦点设置到第一个输入框中，方便用户开始输入
+        private void Form_Load(object sender, EventArgs e)
+        {
+            Task.Delay(1000);
+            txt_input1.Focus();
+        }
+        */
+
+        private void Form_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) { btn_count_Click(sender,e); }
+            if (e.KeyCode == Keys.Delete) { but_clear_Click(sender,e); }
+
+        }
+    }
+}
